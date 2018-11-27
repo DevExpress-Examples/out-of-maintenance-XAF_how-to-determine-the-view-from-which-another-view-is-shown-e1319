fe8837aae8c7c3ecc2ddf9b211ff53b2bd2dca27
Uriahas <chookua@gmail.com>
2018-11-27 19:02:26 +0300

    Optimized usings

--- VB/WinWebSolution.Module/MainWindowController.vb
+++ VB/WinWebSolution.Module/MainWindowController.vb
@@ -1,18 +1,16 @@
-Imports Microsoft.VisualBasic
-Imports System
 Imports DevExpress.ExpressApp
 
 Namespace WinWebSolution.Module
-	Public Class MainWindowController
-		Inherits WindowController
-		Public Sub New()
-			TargetWindowType = WindowType.Main
-		End Sub
-		Protected Overrides Sub OnActivated()
-			MyBase.OnActivated()
-			AddHandler Application.ViewShowing, AddressOf Application_ViewShowing
-			AddHandler Application.ViewShown, AddressOf Application_ViewShown
-		End Sub
+    Public Class MainWindowController
+        Inherits WindowController
+        Public Sub New()
+            TargetWindowType = WindowType.Main
+        End Sub
+        Protected Overrides Sub OnActivated()
+            MyBase.OnActivated()
+            AddHandler Application.ViewShowing, AddressOf Application_ViewShowing
+            AddHandler Application.ViewShown, AddressOf Application_ViewShown
+        End Sub
         Private Sub Application_ViewShowing(ByVal sender As Object, ByVal e As ViewShowingEventArgs)
             If e.SourceFrame IsNot Nothing AndAlso e.SourceFrame.View IsNot Nothing AndAlso e.SourceFrame.View.Id = "Party_PhoneNumbers_ListView" Then
                 Dim targetView As DetailView = CType(e.View, DetailView)
