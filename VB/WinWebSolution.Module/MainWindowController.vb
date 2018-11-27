8cbb02c6e9d1e5fc6245c0077235264b2b525c0f
Uriahas <chookua@gmail.com>
2018-11-27 18:58:48 +0300

    Fixed the NullReferenceException

--- VB/WinWebSolution.Module/MainWindowController.vb
+++ VB/WinWebSolution.Module/MainWindowController.vb
@@ -13,24 +13,24 @@ Namespace WinWebSolution.Module
 			AddHandler Application.ViewShowing, AddressOf Application_ViewShowing
 			AddHandler Application.ViewShown, AddressOf Application_ViewShown
 		End Sub
-		Private Sub Application_ViewShowing(ByVal sender As Object, ByVal e As ViewShowingEventArgs)
-			If e.SourceFrame IsNot Nothing AndAlso e.SourceFrame.View IsNot Nothing AndAlso e.SourceFrame.View.Id = "Party_PhoneNumbers_ListView" Then
-				Dim phoneNumberFrame As Frame = e.TargetFrame
-				Dim phoneNumberDetailView As DetailView = CType(e.View, DetailView)
-				'...
-			End If
-		End Sub
-		'OR
-		Private Sub Application_ViewShown(ByVal sender As Object, ByVal e As ViewShownEventArgs)
-			If e.SourceFrame IsNot Nothing AndAlso e.SourceFrame.View IsNot Nothing AndAlso e.SourceFrame.View.Id = "Party_PhoneNumbers_ListView" Then
-				Dim phoneNumberFrame As Frame = e.SourceFrame
-				Dim phoneNumberDetailView As DetailView = CType(phoneNumberFrame.View, DetailView)
-				'...
-			End If
-		End Sub
-		Protected Overrides Sub OnDeactivated()
-			RemoveHandler Application.ViewShown, AddressOf Application_ViewShown
-			MyBase.OnDeactivated()
-		End Sub
-	End Class
+        Private Sub Application_ViewShowing(ByVal sender As Object, ByVal e As ViewShowingEventArgs)
+            If e.SourceFrame IsNot Nothing AndAlso e.SourceFrame.View IsNot Nothing AndAlso e.SourceFrame.View.Id = "Party_PhoneNumbers_ListView" Then
+                Dim targetView As DetailView = CType(e.View, DetailView)
+                Dim sourceView As ListView = CType(e.SourceFrame.View, ListView)
+                '...
+            End If
+        End Sub
+        'OR
+        Private Sub Application_ViewShown(ByVal sender As Object, ByVal e As ViewShownEventArgs)
+            If e.SourceFrame IsNot Nothing AndAlso e.SourceFrame.View IsNot Nothing AndAlso e.SourceFrame.View.Id = "Party_PhoneNumbers_ListView" Then
+                Dim targetView As DetailView = CType(e.TargetFrame.View, DetailView)
+                Dim sourceView As ListView = CType(e.SourceFrame.View, ListView)
+                '...
+            End If
+        End Sub
+        Protected Overrides Sub OnDeactivated()
+            RemoveHandler Application.ViewShown, AddressOf Application_ViewShown
+            MyBase.OnDeactivated()
+        End Sub
+    End Class
 End Namespace
