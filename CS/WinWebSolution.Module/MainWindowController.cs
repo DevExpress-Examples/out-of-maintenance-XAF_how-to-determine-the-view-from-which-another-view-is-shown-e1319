c58a2b9285cc0198fe9c078e9311234e31e107e6
Uriahas <chookua@gmail.com>
2018-11-27 18:33:01 +0300

    Fixed the InvalidCastException

--- CS/WinWebSolution.Module/MainWindowController.cs
+++ CS/WinWebSolution.Module/MainWindowController.cs
@@ -12,17 +12,17 @@ namespace WinWebSolution.Module {
             Application.ViewShown += new EventHandler<ViewShownEventArgs>(Application_ViewShown);
         }
         void Application_ViewShowing(object sender, ViewShowingEventArgs e) {
-            if (e.SourceFrame != null && e.SourceFrame.View != null && e.SourceFrame.View.Id == "Party_PhoneNumbers_ListView") {
-                Frame phoneNumberFrame = e.TargetFrame;
-                DetailView phoneNumberDetailView = (DetailView)e.View;
+            if(e.SourceFrame != null && e.SourceFrame.View != null && e.SourceFrame.View.Id == "Party_PhoneNumbers_ListView") {
+                DetailView targetView = (DetailView)e.View;
+                ListView sourceView = (ListView)e.SourceFrame.View;
                 //...
             }
         }
         //OR
         void Application_ViewShown(object sender, ViewShownEventArgs e) {
-            if (e.SourceFrame != null && e.SourceFrame.View != null && e.SourceFrame.View.Id == "Party_PhoneNumbers_ListView") {
-                Frame phoneNumberFrame = e.SourceFrame;
-                DetailView phoneNumberDetailView = (DetailView)phoneNumberFrame.View;
+            if(e.SourceFrame != null && e.SourceFrame.View != null && e.SourceFrame.View.Id == "Party_PhoneNumbers_ListView") {
+                DetailView targetView = (DetailView)e.TargetFrame.View;
+                ListView sourceView = (ListView)e.SourceFrame.View;
                 //...
             }
         }
